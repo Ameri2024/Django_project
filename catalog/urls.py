520cf75826c8a797b9938068d3a3762346c308ca
ameri2024 <arabamerimohammad@gmail.com>
2024-03-03 18:54:56 +0330

    my commit

---
+++ catalog/urls.py
@@ -0,0 +1,6 @@
+from django.urls import re_path as url
+
+urlpatterns = [
+    # url(r"^/$", x.as_view(), name=""),
+
+]
--- mysite/settings.py
+++ mysite/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'catalog.apps.AppConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
